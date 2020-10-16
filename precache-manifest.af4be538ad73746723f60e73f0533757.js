acd6902baef576823efcd74eaef48156620350dc
אוהד ליבוביץ <ohad@whds-MacBook-Pro.local>
2020-10-16 22:48:50 +0300

    Updates

---
+++ precache-manifest.af4be538ad73746723f60e73f0533757.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6278731d9083a5aa22fede44d5795c3b",
+    "url": "/blackjack-game/index.html"
+  },
+  {
+    "revision": "e39fdcbf446e16ef087b",
+    "url": "/blackjack-game/static/css/main.55e6aa1e.chunk.css"
+  },
+  {
+    "revision": "69db3f20d82378312f56",
+    "url": "/blackjack-game/static/js/2.b62baa48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/blackjack-game/static/js/2.b62baa48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e39fdcbf446e16ef087b",
+    "url": "/blackjack-game/static/js/main.b6d60240.chunk.js"
+  },
+  {
+    "revision": "384e053ff615f1c1d078",
+    "url": "/blackjack-game/static/js/runtime-main.33de3a83.js"
+  }
+]);
